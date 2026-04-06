26dfde7971b9961a51d937365eb20b3c26e11857
Raphael Camargo <raphaelcamargo42@gmail.com>
2026-04-05 21:18:34 -0300

    Add files via upload

---
+++ dados_primarios_selecoes.sql
@@ -0,0 +1,64 @@
+USE World_Cup_26;
+
+INSERT INTO seleções ( id_selecao, nome, sigla, confederacao, ranking_fifa, grupo)
+values
+-- Grupo A 
+(4734, 'México', 'MEX', 'CONCACAF', 15, 'A'),
+(4740, 'África do Sul', 'RSA', 'CAF', 59, 'A'),
+(4400, 'Coreia do Sul', 'KOR', 'AFC', 22, 'A'),
+(4705, 'Tchéquia', 'CZE', 'UEFA', 40, 'A'),
+-- Grupo B
+(4724, 'Canadá', 'CAN', 'CONCACAF', 40, 'B'),
+(4697, 'Bósnia e Herzegovina', 'BIH', 'UEFA', 74, 'B'),
+(4406, 'Catar', 'QAT', 'AFC', 37, 'B'),
+(4489, 'Suíça', 'SUI', 'UEFA', 19, 'B'),
+-- Grupo C
+(4425, 'Brasil', 'BRA', 'CONMEBOL', 5, 'C'),
+(4741, 'Marrocos', 'MAR', 'CAF', 13, 'C'),
+(4722, 'Haiti', 'HAI', 'CONCACAF', 90, 'C'),
+(4486, 'Escócia', 'SCO', 'UEFA', 39, 'C'),
+-- Grupo D
+(4725, 'EUA', 'USA', 'CONCACAF', 13, 'D'),
+(4823, 'Paraguai', 'PAR', 'CONMEBOL', 56, 'D'),
+(4408, 'Austrália', 'AUS', 'AFC', 24, 'D'),
+(4703, 'Turquia', 'TUR', 'UEFA', 35, 'D'),
+-- Grupo E
+(4482, 'Alemanha', 'GER', 'UEFA', 16, 'E'),
+(4720, 'Curaçao', 'CUW', 'CONCACAF', 91, 'E'),
+(4745, 'Costa do Marfim', 'CIV', 'CAF', 38, 'E'),
+(4819, 'Equador', 'ECU', 'CONMEBOL', 31, 'E'),
+-- Grupo F
+(4492, 'Holanda', 'NED', 'UEFA', 7, 'F'),
+(4412, 'Japão', 'JPN', 'AFC', 18, 'F'),
+(4491, 'Suécia', 'SWE', 'UEFA', 28, 'F'),
+(4755, 'Tunísia', 'TUN', 'CAF', 41, 'F'),
+-- Grupo G
+(4485, 'Bélgica', 'BEL', 'UEFA', 8, 'G'),
+(4744, 'Egito', 'EGY', 'CAF', 36, 'G'),
+(4407, 'Irã', 'IRN', 'AFC', 20, 'G'),
+(4424, 'Nova Zelândia', 'NZL', 'OFC', 103, 'G'),
+-- Grupo H
+(4493, 'Espanha', 'ESP', 'UEFA', 3, 'H'),
+(4742, 'Cabo Verde', 'CPV', 'CAF', 65, 'H'),
+(4411, 'Arábia Saudita', 'KSA', 'AFC', 53, 'H'),
+(4422, 'Uruguai', 'URU', 'CONMEBOL', 11, 'H'),
+-- Grupo I
+(4480, 'França', 'FRA', 'UEFA', 2, 'I'),
+(4754, 'Senegal', 'SEN', 'CAF', 17, 'I'),
+(4405, 'Iraque', 'IRQ', 'AFC', 58, 'I'),
+(4488, 'Noruega', 'NOR', 'UEFA', 47, 'I'),
+-- Grupo J
+(4481, 'Argentina', 'ARG', 'CONMEBOL', 1, 'J'),
+(4691, 'Argélia', 'ALG', 'CAF', 43, 'J'),
+(4711, 'Áustria', 'AUT', 'UEFA', 25, 'J'),
+(4409, 'Jordânia', 'JOR', 'AFC', 71, 'J'),
+-- Grupo K
+(4483, 'Portugal', 'POR', 'UEFA', 6, 'K'),
+(4820, 'Colômbia', 'COL', 'CONMEBOL', 12, 'K'),
+(4413, 'Uzbequistão', 'UZB', 'AFC', 64, 'K'),
+(4746, 'RD Congo', 'COD', 'CAF', 63, 'K'),
+-- Grupo L
+(4438, 'Inglaterra', 'ENG', 'UEFA', 4, 'L'),
+(4494, 'Croácia', 'CRO', 'UEFA', 10, 'L'),
+(4747, 'Gana', 'GHA', 'CAF', 68, 'L'),
+(4717, 'Panamá', 'PAN', 'CONCACAF', 43, 'L');
